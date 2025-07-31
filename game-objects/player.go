c27d13f9e8e1f89da8c8d8b3fbdeb5518ce1526d
N0TM3-1 <bossdumitru847@gmail.com>
2025-07-31 18:27:28 +0300

    Created a tilemap

--- game-objects/player.go
+++ game-objects/player.go
@@ -17,3 +17,30 @@ embedded_components {
     y: 5.0
   }
 }
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"player\"\n"
+  "mask: \"walls\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "    id: \"box\"\n"
+  "  }\n"
+  "  data: 80.0\n"
+  "  data: 80.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "locked_rotation: true\n"
+  ""
+}
